cd0207f02417bc87b6b07b2606ec9bc0b312fab1
cajimenez96 <carlosj_utn@hotmail.com>
2022-11-17 21:41:05 -0300

    Clase 17/11 REPASO GRAL.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Tabs from './components/Tabs/Tabs';
+import Home from './views/Home/Home';
+import PageElement from './views/PageElement/PageElement';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Navbar title='The Star Wars API' />
+        <Tabs />
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/people' element={<PageElement element='people' />} />
+          <Route path='/planets' element={<PageElement element='planets' />} />
+          <Route path='/starships' element={<PageElement element='starships' />} />
+          <Route path='/species' element={<PageElement element='species' />} />
+          <Route path='/vehicles' element={<PageElement element='vehicles' />} />
+          <Route path='/films' element={<PageElement element='films' />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
